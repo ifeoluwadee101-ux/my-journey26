47d5695025d0b3d9a68d86b45fc499c30c5a6a4a
ifeoluwadee101-ux <ifeoluwadee101@gmail.com>
2026-03-05 00:02:15 +0700

    introduction to python input function

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 # print("ifeoluwa is learning python")
 # print("ifeoluwa wants to become a python expert")
 
-name ="ifeoluwa"
-language = "c#"
+name = input("enter your name: ")
+language = input("preferred language: ")
 print("my name is",name)
 print(name,"is learning",language)
 print(name,"wants to become a",language,"expert")
