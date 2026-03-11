4d3117a0fa786cf83b66152e9ba40584c5dcb57f
ifeoluwadee101-ux <ifeoluwadee101@gmail.com>
2026-03-12 00:05:10 +0700

    learning how to add

--- strings101.py
+++ strings101.py
@@ -1,13 +1,20 @@
-sentence = """i love Python.
-Python is very easy, Python is also very
-powerful. AI depends mostly on Python.
-Python can be used for.
-- AI
-- Web Design
-- Automations
-pgit addython is a highly versatile language
- """
-print(sentence.replace('python','java').replace('python','java'))
+# sentence = """i love Python.
+# Python is very easy, Python is also very
+# powerful. AI depends mostly on Python.
+# Python can be used for.
+# - AI
+# - Web Design
+# - Automations
+# pgit addython is a highly versatile language
+#  """
+# print(sentence.replace('python','java').replace('python',''))
 
 # eurodate = "2026/03/11"
 #print(eurodate.replace("/","-"))
+name = "dee"
+surname = "ife"
+age = 35
+
+fullname =name+' '+surname
+# print ("my name is ",fullname+" i am "+str(age)+ " years old")
+print(f"my name is {fullname}.I am {age} years old.\nIn the next ten years, i will be {age+10} years old")
