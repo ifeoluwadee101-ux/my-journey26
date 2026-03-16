0849b88b9fc6e1f133e0f2952b1a8fceedf70adc
ifeoluwadee101-ux <ifeoluwadee101@gmail.com>
2026-03-16 13:16:46 +0700

    i modified data_types.py and learned new things in strings101.py

--- data_types.py
+++ data_types.py
@@ -1,25 +1,30 @@
-a = "Hello"
-b = 'Emmanuel'
-c = 16
-d = 3.56
-e = """198"""
-f = True
-g = [1,2,"Emily",True]
-h = None
+#python challenge
+#create 5 variables -each with a different data type
+age = 19
+height = 3.25
+name = "dee"
+is_student = True
+eaten = None
+name_of_friends = ["dee", "loviee", "marvel"]
 
-#print(b.upper())
-#print(b.lower())
-print(c.bit_length())
-
-
-
-
-#print(f"this variables contains {len(g)} items")
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# print(type(d))
-# print(type(e))
-# print(type(f))
-# print(type(g))
-# print(type(h))
+print(f"i am {age} years old")
+print(f"age is a {type(age)} data type")
+print(f"my age contains {age.bit_length()} bits")
+print()
+print(f"my height is {height} meters")
+print(f"height is a {type(height)} data type")
+print(f"my height contains{height.hex()} bits")
+print()
+print(f"my name is {name}")
+print(f"name is a {type(name)} data type")
+print(f"my name contains{len(name)} characters")
+print()
+print(f"i am a student: {is_student}")
+print(f"is_student is a {type(is_student)} data type")
+print()
+print(f"i have eaten: {eaten} today")
+print(F"eaten is a {type(eaten)} data type")
+print()
+print(f"my friends are {name_of_friends}")
+print(f"name of friends is{type(name_of_friends)} data type")
+print(f"my name_of_friends contain {len(name_of_friends)} item")
--- strings101.py
+++ strings101.py
@@ -10,11 +10,27 @@
 # print(sentence.replace('python','java').replace('python',''))
 
 # eurodate = "2026/03/11"
-#print(eurodate.replace("/","-"))
-name = "dee"
-surname = "ife"
-age = 35
+# #print(eurodate.replace("/","-"))
+# name = "dee"
+# surname = "ife"
+# age = 35
 
-fullname =name+' '+surname
-# print ("my name is ",fullname+" i am "+str(age)+ " years old")
-print(f"my name is {fullname}.I am {age} years old.\nIn the next ten years, i will be {age+10} years old")
+# fullname =name+' '+surname
+# # print ("my name is ",fullname+" i am "+str(age)+ " years old")
+# print(f"my name is {fullname}.I am {age} years old.\nIn the next ten years, i will be {age+10} years old")
+
+
+# timestamp1 = '2026-12-23'
+
+# customerinfo = "mary-23-female-kogi"
+# # print(customerinfo)
+# print(customerinfo.split("-"))
+# name, age,gender,state = customerinfo.split("-")
+# print(f"""customer name: {name}
+#            age:{age}
+#            gender:{gender}
+#            state of origin:{state}
+#     """)
+csvfile = "Rice,Naira,Africa,Market"
+
+csvfile.split(',')
